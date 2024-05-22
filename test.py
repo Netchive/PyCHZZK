ba27a43059120fdf56899ef78dbc96a440862113
Euiseo Cha <zeroday0619_dev@outlook.com>
2024-05-22 19:18:09 +0900

    feat: rm test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import json
-import asyncio
-from PyCHZZK.api import Channels
-
-
-if __name__ == "__main__":
-    channel = Channels()
-    res = asyncio.run(channel.search("녹두로"))
-    print(json.dumps(res, indent=4, ensure_ascii=False))
