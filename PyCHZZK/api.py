e3fc5e6b32f37fc0c7b7fd9e6369daf142d8bcea
Euiseo Cha <zeroday0619_dev@outlook.com>
2024-05-22 19:14:45 +0900

    feat: write test case and add pyproject.toml

--- PyCHZZK/api.py
+++ PyCHZZK/api.py
@@ -1,3 +1,4 @@
+from .enums import Fields
 from .utils import null_check
 from ._http import HTTP
 
@@ -12,6 +13,27 @@ class Channels(HTTP):
         )
     
     async def search(self, keyword: str) -> list[dict]:
+        """Search channels
+
+        example:
+            ```python
+            import asyncio
+            from PyCHHZK.api import Channels
+
+            async def print_search_channels():
+                channel = Channels()
+                channels = await channel.search("녹두로")
+                print(channels)
+            
+            asyncio.run(print_search_channels())
+            ```
+        
+        Args:
+            keyword (str): Search keyword
+        
+        Returns:
+            dict: Search result
+        """
         response = await self.fetch("GET", "search/channels", params={
             "keyword": keyword
         })
@@ -19,3 +41,34 @@ class Channels(HTTP):
         content = null_check(raw_data.get("content"))
         data = null_check(content.get("data"))
         return data
+    
+    async def get_data(self, channel_id: str, fields: Fields) -> dict:
+        """Get channel data
+
+        example:
+            ```python
+            import asyncio
+            from PyCHHZK.api import Channels
+            from PyCHZZK.enums import Fields
+            
+            async def print_channel_data():
+                channel = Channels()
+                channel_data = await channel.get_data(channel_id, Fields.description)
+                print(channel_data)
+            
+            asyncio.run(print_channel_data())
+            ```
+
+        Args:
+            channel_id (str): Channel ID
+            fields (Fields): Fields
+        
+        Returns:
+            dict: Channel data
+        """
+        response = await self.fetch("GET", f"channels/{channel_id}/data", params={
+            "fields": fields.value
+        })
+        raw_data = response.json()
+        content = null_check(raw_data.get("content"))
+        return content
